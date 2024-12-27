bf1384f2d82594c1dcdc45e9165ef30b1031c38b
chuudino <chuudino@gmail.com>
2024-12-27 10:38:17 +0800

    fix

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from utils import init_page
 
 init_page()
 st.title("這是首頁")
-
-st.set_page_config(page_title="Dino's website", layout="wide")
 st.title("Welcome~")  # 這是標題
 
 # to view this Streamlit app on a browser, run it with the following
---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+import streamlit as st
+from .utils import init_page
+from .menu import menu
