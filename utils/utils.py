64bb0421bddfbe6494304a467a1c773c7506a3ae
chuudino <chuudino@gmail.com>
2024-12-27 11:33:42 +0800

    update

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,22 @@
+import os
+import dotenv
 import streamlit as st
 from .menu import menu
 
+dotenv.load_dotenv()
+
 
 def init_page():
+    DEV = os.getenv("dev")
+    if DEV == "True":
+        import subprocess
+
+        try:
+            subprocess.run(
+                ["pipreqs", ".", "--force"], check=True, text=True, capture_output=True
+            )
+        except Exception as e:
+            print(f"命令執行失敗：\n{e}")
+
     st.set_page_config(page_title="Dino's website", page_icon="🦖", layout="wide")
     menu()
